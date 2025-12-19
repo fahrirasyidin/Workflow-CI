ced057a49e38bddf4edc935fba740c399e1aaab5
fahrirasyidin <fahrirasyidin51@gmail.com>
2025-12-19 09:18:15 +0700

    Update

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 def train():
-    # 1. Load Data
+    # Load Data
     data_path = "dataset/transportation_data_processed.csv"
     if not os.path.exists(data_path):
         print("Error: File data tidak ditemukan.")
@@ -18,18 +18,17 @@ def train():
     y = df['Accident_Occurred']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # 2. Hapus folder output lama jika ada (Penting untuk Docker Build)
+    # Hapus folder output lama jika ada
     model_output_path = "model_output"
     if os.path.exists(model_output_path):
         shutil.rmtree(model_output_path)
 
-    # 3. Training
+    # Training
     print("Training model...")
     model = RandomForestClassifier(n_estimators=50, random_state=42)
     model.fit(X_train, y_train)
 
-    # 4. Simpan Model ke Folder Lokal (Syarat Advance)
-    # Ini agar perintah 'mlflow models build-docker' bisa membaca modelnya
+    # Simpan Model ke Folder Lokal
     mlflow.sklearn.save_model(model, model_output_path)
     
     print(f"Model berhasil disimpan di folder: {model_output_path}")
