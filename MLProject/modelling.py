04d7a46fbe50697227c65e7c635b71e9a5b750b5
fahrirasyidin <fahrirasyidin51@gmail.com>
2025-12-23 02:05:37 +0700

    update file

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,37 +1,39 @@
 import pandas as pd
 import mlflow
-import mlflow.sklearn
 import os
-import shutil
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 def train():
+    mlflow.autolog()
+
     # Load Data
-    data_path = "dataset/transportation_data_processed.csv"
+    data_path = "transportation_data_processed.csv" 
+    
     if not os.path.exists(data_path):
-        print("Error: File data tidak ditemukan.")
+        print(f"Error: File {data_path} tidak ditemukan.")
         return
 
     df = pd.read_csv(data_path)
+    
+    # Pisahkan Fitur dan Target
     X = df.drop(columns=['Accident_Occurred'])
     y = df['Accident_Occurred']
+
+    # Split Data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Hapus folder output lama jika ada
-    model_output_path = "model_output"
-    if os.path.exists(model_output_path):
-        shutil.rmtree(model_output_path)
+    # MLflow Run
+    with mlflow.start_run():
+        print("Training model...")
+        
+        # Inisialisasi Model
+        model = RandomForestClassifier(n_estimators=50, random_state=42)
+        
+        # Training Model
+        model.fit(X_train, y_train)
 
-    # Training
-    print("Training model...")
-    model = RandomForestClassifier(n_estimators=50, random_state=42)
-    model.fit(X_train, y_train)
-
-    # Simpan Model ke Folder Lokal
-    mlflow.sklearn.save_model(model, model_output_path)
-    
-    print(f"Model berhasil disimpan di folder: {model_output_path}")
+        print("Training selesai")
 
 if __name__ == "__main__":
     train()
